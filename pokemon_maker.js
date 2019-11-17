fc6efaf43f8a12a45dc98a6ddeb893a4983e0c1a
Daniel Ting <professorredstone917@gmail.com>
2019-11-17 02:34:45 -0600

    Add Wolfram Alpha

--- pokemon_maker.js
+++ pokemon_maker.js
@@ -1,11 +1,14 @@
 names = ["Ben Wang", "Pranay", "Daniel", "Eb", "prydt", "tiffaniffaniff", "marshmallow", "polymatter", "3b1b", "tams", "orange", "Austin", "Dustin", "Choinut", "WBNE", "Minecraft", "cup", "table", "fork", "computer", "UwU-man", "OwO-woman", "shorty", "tally", "Wolfie", "Tookie", "Foxie", "Brad", "Pitt", "Ganon", "Roy", "Kirby", "Firey", "R.O.B", "Sonic", "horse", "Ben Taylor", "Ben Sun", "Kelly Liu", "CarDar", "Socks", "Dongle", "Grasshawk", "Rock", "Jingle", "Bucket", "Watermelon", "Truck", "Boomer", "Hack", "Hellong", "Jay Park", "Evan Carlisie", "Anthony Zhou", "Preston Wong" ] 
 
 const app = document.getElementById('root')
+const NUMBER = 6;
+var foo = 0;
 
 //NEW
 pickedNames = []
+var averages = [0, 0, 0, 0, 0, 0] // Average hitpoints, attack, defense, special attack, special defense, and speed
 
-for(i = 1; i <= 6; i++){
+for(var i = 1; i <= NUMBER; i++){
   var request = new XMLHttpRequest()
   pokemon_name = ""
   pokemon_description = ""
@@ -29,15 +32,22 @@ for(i = 1; i <= 6; i++){
   request.open('GET', randomString, true)
   request.onload = function() {
   var data = JSON.parse(this.response)
-
+  // Update averages
+  averages[0] += data.stats[5]['base_stat'];
+  averages[1] += data.stats[4]['base_stat'];
+  averages[2] += data.stats[3]['base_stat'];
+  averages[3] += data.stats[2]['base_stat'];
+  averages[4] += data.stats[1]['base_stat'];
+  averages[5] += data.stats[0]['base_stat'];
+  
   pokemon_name = data.name
   const h33 = document.createElement('h3')
   h33.textContent = pokemon_name
   card.appendChild(h33)
   h33.setAttribute('class', 'thicc')
 
-   for(i = 1; i <= data.types.length; i++){
-	  pokemon_type = "Type " + i + ": " + data.types[i - 1].type.name + " "
+   for(var j = 1; j <= data.types.length; j++){
+	  pokemon_type = "Type " + j + ": " + data.types[j - 1].type.name + " "
           const ulul = document.createElement('ul')
           ulul.setAttribute('class', 'thiccc')
           ulul.textContent = pokemon_type
@@ -60,7 +70,7 @@ for(i = 1; i <= 6; i++){
     moves.textContent = "Moves: "
     moves.setAttribute('class', 'bottom-text')
     card.appendChild(moves)
-      for(i = 0; i < 4; i++){
+      for(var k = 0; k < 4; k++){
         randomNum = Math.floor(Math.random() * data.moves.length)
         const ul = document.createElement('ul')
         ul.textContent = data.moves[randomNum].move.name
@@ -69,8 +79,12 @@ for(i = 1; i <= 6; i++){
       /*const errorMessage = document.createElement('marquee')
       errorMessage.textContent = `Gah, it's not working!`
       app.appendChild(errorMessage) 
-	If I want to add errors uwu*/
+  If I want to add errors uwu*/
+  
+  if (++foo === NUMBER) {
+    wolfram();
   }
+}
   app.appendChild(card) 
   request.send()
 }
@@ -78,7 +92,7 @@ for(i = 1; i <= 6; i++){
 const flavor = document.getElementById('flavor')
 potato = ""
 lameString = ["A", "b", "c", "d", "e", "f"]
-for(i = 0; i < 6; i++) {
+for(i = 0; i < NUMBER; i++) {
 	dumbdumb = pickedNames[i]
 	randomNumber = Math.floor((Math.random() * 30) + 1)
 	randomString = 'https://pokeapi.co/api/v2/characteristic/' + randomNumber
@@ -93,7 +107,7 @@ reeee.send()
 work()
 }
 function work() {
-for(i = 0; i < 6; i++) {
+for(i = 0; i < NUMBER; i++) {
 	fl = document.createElement('p')
 	fl.setAttribute('class', 'flavor-text')
 	fl.textContent = (pickedNames[i] + ": " + lameString[i])
@@ -115,4 +129,22 @@ function logMouseOver() {
 
 function logMouseOut() {
   document.getElementById('button').style.background = '#002b36';
+}
+
+const mix = document.createElement('div')
+mix.setAttribute('class', 'card')
+
+// Wolfram
+function wolfram() {
+  for (var i = 0; i < NUMBER; i++)
+  {
+    averages[i] /= NUMBER;
+  }
+  var string = averages.join(',')
+  var request = new XMLHttpRequest()
+  request.open('GET', "https://www.wolframcloud.com/obj/1a00bd3c-f2ea-4472-8561-d2d73b5fa6af?x=" + string, true)
+  request.onload = function() {
+    var closest = this.responseText.substring(this.responseText.indexOf(':') + 1, this.responseText.length - 2)
+  }
+  request.send()
 }
